af1cdda83f54eb6ffc26bfd7d7a841827b3183cc
Arjunsreddy <139111755+Arjunsreddy@users.noreply.github.com>
2023-08-22 15:19:14 +0530

    Add files via upload

---
+++ terraform/create_nexusinstance/nexus.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+sudo su -
+yum update -y
+yum install docker -y
+systemctl start docker
+systemctl enable docker
+docker run -d --restart always -p 8081:8081 --name nexus sonatype/nexus3
+
